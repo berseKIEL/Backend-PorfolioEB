3d9cc6f01fc189d7ca311ed1284107dc98bf09d2
bersekiel <exebarco280320@gmail.com>
2023-04-15 01:38:54 -0300

    [ADD] change models to porfolio

--- src/models/porfolio.py
+++ src/models/porfolio.py
@@ -1,8 +1,31 @@
 from pydantic import BaseModel
+from typing import List, Dict
+from datetime import datetime
 
 class Porfolio(BaseModel):
-    description: str
-    title: str
+    # Login
+    username: str
     email: str
+    password: str
+    
+    # Normal Data
+    first_name: str
+    last_name: str
+    title: Dict[str, str]
     phone: str
-    location: str
+    description: str
+    county: str
+    province: str
+        
+    # Porfolio
+    projects: List[Dict[str, str]]
+    skills: List[Dict[str, str]]
+    
+    # Creation
+    date_creation: datetime
+    date_modified: datetime
+    
+    class Config:
+        json_encoders = {
+            ObjectId: str
+        }
